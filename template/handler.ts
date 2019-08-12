bb0e362ed3aca63b3aee36dd01b7b5f44fc358b8
Gerard Rey Echiverri <gerard@idearobin.com>
2019-08-12 20:36:17 +0800

    fix: 🐛 cors

--- template/handler.ts
+++ template/handler.ts
@@ -2,9 +2,15 @@ import { APIGatewayProxyHandler } from 'aws-lambda'
 
 import { create as createRecord } from './model'
 
+const headers = {
+  'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+  'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+}
+
 export const list: APIGatewayProxyHandler = async event => {
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify(
       {
         message: `Life's a peach, eat more apples!`,
@@ -21,6 +27,7 @@ export const create: APIGatewayProxyHandler = async () => {
 
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify(
       {
         record,
