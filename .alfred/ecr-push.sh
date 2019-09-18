113c5231823ca7d8d5a5322c7c068afcbf41d2df
Gerard Rey Echiverri <gerard@idearobin.com>
2019-09-18 19:08:50 +0800

    feat: 🎸 add versioning in sitrep

--- .alfred/ecr-push.sh
+++ .alfred/ecr-push.sh
@@ -6,6 +6,8 @@ GIT_REPO_NAME=$(cat .alfred/git-repo-name.txt)
 
 BUILD_LINK="<'$BUILD_URL'|Build #'$BUILD_NUMBER'>"
 
+echo '{"stage": "'$JOB_BASE_NAME'","version": "'$COMMIT_SHA'"}' > src/services/general/version.json
+
 docker rm -f ${GIT_REPO_NAME}-${JOB_BASE_NAME} &> /dev/null
 docker rmi ${GIT_REPO_NAME}-${JOB_BASE_NAME} &> /dev/null
 docker build -t ${GIT_REPO_NAME}-${JOB_BASE_NAME} -f Dockerfile.serve .
--- src/services/general/handler.ts
+++ src/services/general/handler.ts
@@ -1,12 +1,19 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
+import fs from 'fs'
+import { promisify } from 'util'
 import 'source-map-support/register'
 
 export const sitrep: APIGatewayProxyHandler = async event => {
+  const readFileAsync = promisify(fs.readFile)
+  const json = await readFileAsync(__dirname + '/version.json', 'utf8')
+  const fingerprint = JSON.parse(json)
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: `Life's a peach, eat more apples!`,
+        fingerprint,
         input: event,
       },
       null,
