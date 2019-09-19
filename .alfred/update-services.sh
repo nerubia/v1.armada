df5f99c14018e305419decc36c61621e0f810932
Gerard Rey Echiverri <gerard@idearobin.com>
2019-09-19 14:53:52 +0800

    ci: 🎡 refactor services

--- .alfred/update-services.sh
+++ .alfred/update-services.sh
@@ -6,13 +6,13 @@ case $BRANCH_NAME in
     ;;
 esac
 
-COMMIT_SHA=$(cat .alfred/git-commit-short.txt)
-GIT_REPO_NAME=$(cat .alfred/git-repo-name.txt)
-ROOT_DIR=$(pwd)
+export COMMIT_SHA=$(cat .alfred/git-commit-short.txt)
+export GIT_REPO_NAME=$(cat .alfred/git-repo-name.txt)
+export ROOT_DIR=$(pwd)
 
 curl -X POST -s $SLACK_URL -d '{
   "type": "mrkdwn",
-  "text": "Pushing Image",
+  "text": "Updating Lambdas",
   "blocks": [
     { "type": "divider" },
     {
@@ -20,10 +20,10 @@ curl -X POST -s $SLACK_URL -d '{
       "accessory": {
         "type": "image",
         "image_url": "https://greative-assets.s3.amazonaws.com/octocats/hulatocat.gif",
-        "alt_text": "Updating services"
+        "alt_text": "Updating Lambdas"
       },
       "fields": [
-        { "type": "mrkdwn", "text": "*Stage:* Updating lambdas" },
+        { "type": "mrkdwn", "text": "*Stage:* Updating Lambdas" },
         { "type": "mrkdwn", "text": "*Build:* <'$BUILD_URL'/console|'$BUILD_NUMBER'>" },
         { "type": "mrkdwn", "text": "*Project:* '$GIT_REPO_NAME'" },
         { "type": "mrkdwn", "text": "*Branch:* '$JOB_BASE_NAME'" }
@@ -36,12 +36,8 @@ curl -X POST -s $SLACK_URL -d '{
   ]
 }'
 
-cp .env src/services/general
-echo "export default '"$COMMIT_SHA"'" > version.ts
-cd src/services/general
-
-echo "STAGE="$STAGE >> .env
-
-docker build -t ${GIT_REPO_NAME}-${JOB_BASE_NAME}-general .
-docker run --rm --env-file .env ${GIT_REPO_NAME}-${JOB_BASE_NAME}-general
-docker rmi ${GIT_REPO_NAME}-${JOB_BASE_NAME}-general
+sh .alfred/services/general.sh
+curl -X POST -s $SLACK_URL -d '{
+  "type": "mrkdwn",
+  "text": "<'$BUILD_URL'/console|'$BUILD_NUMBER'>. *Updated all Lambdas!*"
+}'
