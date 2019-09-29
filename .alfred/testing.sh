ba0a45c1a0719dead2f98379eb4447c3fe8a8bcc
g-six <gerard@idearobin.com>
2019-09-29 01:03:32 +0000

    ci: terminate on error by throwing caught error

--- .alfred/testing.sh
+++ .alfred/testing.sh
@@ -28,8 +28,8 @@ curl -X POST -s $SLACK_URL -d '{
     }
   ]
 }' &> /dev/null &
-docker build -t $IMAGE_NAME .
-docker run --rm --name ${IMAGE_NAME} ${IMAGE_NAME}
+docker build -t $IMAGE_NAME . > testing-build.log
+docker run --rm --name ${IMAGE_NAME} ${IMAGE_NAME} . > testing-run.log
 
 curl -X POST -s $SLACK_URL -d '{
   "type": "mrkdwn",
