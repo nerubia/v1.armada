36cc79fde34beaf4fdbae039ac9034d6dda46257
g-six <gerard@idearobin.com>
2019-10-04 18:58:07 +0800

    refactor: 💡 remove config codes that no longer applies

--- src/services/goc/config.ts
+++ src/services/goc/config.ts
@@ -1,24 +1,4 @@
-import SSM from 'aws-sdk/clients/ssm'
-
-interface SSMParameter {
-  Name: string
-  Value: string
-}
-interface SSMParameters {
-  Parameters: SSMParameter[]
-}
-
 export default async (Names: string[]) => {
-  if (process.env.NODE_ENV === 'local') {
-    return {
-      host: process.env.PGHOST,
-      port: process.env.PGPORT,
-      database: process.env.PGDATABASE,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-    }
-  }
-
   if (process.env.NODE_ENV === 'test') {
     return {
       host: 'localhost',
@@ -28,20 +8,11 @@ export default async (Names: string[]) => {
       password: 'test_password',
     }
   }
-  const ssm = new SSM()
-  const getParameters = ssm.getParameters({ Names })
-
-  const response: SSM.Types.GetParametersResult = await getParameters.promise()
-
-  const { Parameters } = response as SSMParameters
-  const values = {}
-  Parameters.forEach(({ Name, Value }) => {
-    if (Name === 'PGHOST') values['host'] = Value
-    if (Name === 'PGPORT') values['port'] = Value
-    if (Name === 'PGDATABASE') values['database'] = Value
-    if (Name === 'PGUSER') values['user'] = Value
-    if (Name === 'PGPASSWORD') values['password'] = Value
-  })
-
-  return values
+  return {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+  }
 }
