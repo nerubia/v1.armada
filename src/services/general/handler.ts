3e2923e56c98578f4380bf88066bf59465943500
g-six <gerard@idearobin.com>
2019-12-23 08:24:27 +0000

    refactor: 💡 no need for models in SITREP

--- src/services/general/handler.ts
+++ src/services/general/handler.ts
@@ -1,41 +1,4 @@
-import { HttpStatus } from '@g-six/kastle-router'
-import { loadLocale } from '@g-six/swiss-knife'
-import { APIGatewayProxyHandler, APIGatewayProxyEvent } from 'aws-lambda'
-import getValue from 'lodash/get'
-import pick from 'lodash/pick'
 import version from './version'
-import {
-  connectDb,
-  closeDb,
-  create as createRecord,
-  list as listRecords,
-  retrieve as retrieveRecord,
-  update as updateRecord,
-  verifyUser,
-} from './model'
-
-import { Response } from './types'
-
-const headers = {
-  'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-  'Access-Control-Allow-Headers': 'kasl-key',
-  'Access-Control-Expose-Headers': 'kasl-key',
-  'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-  'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-}
-
-const errorResponse = (code: number, error: string, errors?: {}): Response => ({
-  body: JSON.stringify(
-    {
-      error,
-      errors,
-    },
-    null,
-    2,
-  ),
-  headers,
-  statusCode: code || 500,
-})
 
 export const index = async () => {
   return {
