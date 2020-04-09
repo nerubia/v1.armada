09ac95864aab95277bc393696b716438fd5887bf
g-six <gerard@idearobin.com>
2020-04-09 06:38:41 +0000

    feat: 🎸 get ip

--- src/services/sitrep/handler.ts
+++ src/services/sitrep/handler.ts
@@ -1,11 +1,18 @@
+import axios from 'axios'
 import version from './version'
 
 export const sitrep = async () => {
+  const { data } = await axios.get('http://ifconfig.co', {
+    headers: { 'content-type': 'application/json' },
+  })
+  console.log(data)
+  const whitelist_ip = 'asd'
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: `Life's a peach, eat more apples!`,
+        whitelist_ip,
         version,
       },
       null,
